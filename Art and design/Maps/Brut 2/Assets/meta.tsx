99dbb78eb43753b655a3890ba75d3d0b3ca145f5
Nuria Lamonja @needlesslord <needlesslord@gmail.com>
2019-10-24 21:37:32 +0200

    brut2 added

---
+++ Art and design/Maps/Brut 2/Assets/meta.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="meta" tilewidth="64" tileheight="64" tilecount="2" columns="2">
+ <image source="../../../../../Downloads/Dev07_Handout/Dev07_Handout/Game/maps/meta.png" width="128" height="64"/>
+</tileset>
