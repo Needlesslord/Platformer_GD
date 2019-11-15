4c1a4ed9a39a43b35ab109d74d50094ecd12e471
tomascarreras1000 <tomascarreras1000@gmail.com>
2019-11-15 20:31:00 +0100

    Fixing F1/F2 in progress

--- ENTREGA1/Game/Motor2D/j1Scene.cpp
+++ ENTREGA1/Game/Motor2D/j1Scene.cpp
@@ -76,26 +76,24 @@ bool j1Scene::Update(float dt) {
 
 	if (App->input->GetKey(SDL_SCANCODE_F1) == KEY_DOWN) {
 		if (current_scene != 1) {
-			App->map->CleanUp();
 			current_scene = 1;
 			App->player->current_map = 1;
 			App->player->position.x = App->player->originalPosition_1.x;
 			App->player->position.y = App->player->originalPosition_1.y;
 			App->player->velocity.y = 0;
 			App->map->Start();
+			App->scene->Start();
 		}
 	}
 	if (App->input->GetKey(SDL_SCANCODE_F2) == KEY_DOWN) {
 		if (current_scene != 2) {
-			App->map->CleanUp();
-	
 			current_scene = 2;
 			App->player->current_map = 2;
 			App->player->position.x = App->player->originalPosition_2.x;
 			App->player->position.y = App->player->originalPosition_2.y;
 			App->player->velocity.y = 0;
+			App->scene->Start();
 			App->map->Start();
-			
 		}
 	}
 
