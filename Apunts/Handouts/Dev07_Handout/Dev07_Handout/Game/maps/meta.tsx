68964c33c36a0ab6cc85316b0286c45e51981863
Nuria Lamonja @needlesslord <needlesslord@gmail.com>
2019-11-19 19:06:01 +0100

    clean folders

--- Apunts/Handouts/Dev07_Handout/Dev07_Handout/Game/maps/meta.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="meta" tilewidth="64" tileheight="32" tilecount="4" columns="2">
- <image source="meta.png" width="128" height="64"/>
-</tileset>
