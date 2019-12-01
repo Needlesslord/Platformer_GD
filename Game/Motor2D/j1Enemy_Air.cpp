73398a4f155e46a2a1df9809da087313b384578a
tomascarreras1000 <tomascarreras1000@gmail.com>
2019-12-01 23:37:47 +0100

    Never mind

--- Game/Motor2D/j1Enemy_Air.cpp
+++ Game/Motor2D/j1Enemy_Air.cpp
@@ -8,7 +8,9 @@
 #include <cmath>
 
 
-j1Enemy_Air::j1Enemy_Air(ENTITY_TYPE type, float x, float y) : j1Entity(ENTITY_TYPE::ENEMY_AIR, x, y) {}
+j1Enemy_Air::j1Enemy_Air(ENTITY_TYPE type, float x, float y) : j1Entity(ENTITY_TYPE::ENEMY_AIR, x, y)
+{
+}
 
 j1Enemy_Air::~j1Enemy_Air() {};
 
@@ -53,7 +55,7 @@ bool j1Enemy_Air::Update(float dt) {
 
 	//MoveIdle(position);
 
-	if (!HasToAttack(attack_radar_distance)) {
+	if (!HasToAttack()) {
 		distance_air = 0;
 		move_direction_air = 1;
 		if (distance_air > 30) {
@@ -70,7 +72,7 @@ bool j1Enemy_Air::Update(float dt) {
 	}
 
 
-	if (HasToAttack(attack_radar_distance)) AttackPlayer(App->entity_manager->enemy_air->initial_position_enemy_air);
+	if (HasToAttack()) AttackPlayer(App->entity_manager->enemy_air->initial_position_enemy_air);
 
 
 	//if (App->player->col != nullptr) {
@@ -167,8 +169,8 @@ void j1Enemy_Air::OnCollision(Collider* c1, Collider* c2)
 }
 
 void j1Enemy_Air::MoveIdle(iPoint position) {
-	float attack_radar_distance = 20;
-	if (!HasToAttack(attack_radar_distance)) {
+	
+	if (!HasToAttack()) {
 		distance_air = 0;
 		move_direction_air = 1;
 		if (distance_air > 30) {
@@ -196,13 +198,13 @@ void j1Enemy_Air::AttackPlayer(iPoint initial_position_enemy_air) {
 	if (App->player->position.y > position.y) position.y++;
 }
 
-bool j1Enemy_Air::HasToAttack(float attack_radar_distance) {
+bool j1Enemy_Air::HasToAttack() {
 	bool ret = false;
 
-	if ((App->player->position.x > position.x) && ((App->player->position.x - position.x) < attack_radar_distance)) ret = true;
-	else if ((App->player->position.x < position.x) && ((App->player->position.x - position.x) > -attack_radar_distance)) ret = true;
-	if ((App->player->position.y > position.y) && ((App->player->position.y - position.y) < attack_radar_distance)) ret = true;
-	else if ((App->player->position.y < position.y) && ((App->player->position.y - position.y) > -attack_radar_distance)) ret = true;
+	if ((App->player->position.x > position.x) && ((App->player->position.x - position.x) < 20)) ret = true;
+	else if ((App->player->position.x < position.x) && ((App->player->position.x - position.x) > -20)) ret = true;
+	if ((App->player->position.y > position.y) && ((App->player->position.y - position.y) < 20)) ret = true;
+	else if ((App->player->position.y < position.y) && ((App->player->position.y - position.y) > -20)) ret = true;
 
 	return ret;
 }
