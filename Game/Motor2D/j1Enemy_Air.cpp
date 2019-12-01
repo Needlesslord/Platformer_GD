68e2e19e1f9c1338ff1d86d04f7ced560abe29fd
Nuria Lamonja @needlesslord <needlesslord@gmail.com>
2019-12-01 23:39:52 +0100

    trying pathfinding

--- Game/Motor2D/j1Enemy_Air.cpp
+++ Game/Motor2D/j1Enemy_Air.cpp
@@ -54,7 +54,7 @@ bool j1Enemy_Air::Update(float dt) {
 	//MoveIdle(position);
 
 	if (!HasToAttack(attack_radar_distance)) {
-		distance_air = 0;
+		//distance_air = 0;
 		move_direction_air = 1;
 		if (distance_air > 30) {
 			move_direction_air = -1;
@@ -67,6 +67,7 @@ bool j1Enemy_Air::Update(float dt) {
 			if (move_direction_air > 0) distance_air--;
 			else distance_air++;
 		}
+
 		if (distance_air > 30 && !initial_position_enemy_air.x) int i = 0;
 		else if (position.x == initial_position_enemy_air.x)int i = 0;
 	}
